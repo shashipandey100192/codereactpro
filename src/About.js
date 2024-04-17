e5422c16b25c56f863cf2ab64352dcfc6c8d2e1a
shashiweb <shashiweb64@gmail.com>
2024-04-17 17:49:36 +0530

    create new project

---
+++ src/About.js
@@ -0,0 +1,27 @@
+import React,{ Fragment } from "react"
+import { Mycontact2 } from "./Contactus";
+import myimg from "./images/img1.png";
+
+ const a = "kumar ";
+ const  myimgonline = "https://img.freepik.com/free-photo/galactic-night-sky-astronomy-science-combined-generative-ai_188544-9656.jpg";
+function Myabout()
+{
+    return(
+        <Fragment>
+            <h1>welcome  to react js</h1>
+            <div >skdjfjksdh fjk hsdjkfhsdname is: {a} </div>
+            <div >skdjfjksdh fjk hsdjkfhsdname is: {<h4>ppppppppppppppppp</h4>} </div>
+            <div >ooooooooooooooooooooooo is: {<Mycontact2/>} </div>
+            <h1>display image from src folder</h1>
+            <img src={myimg} alt="imagessdfdsf" width={500}/>
+            <h1>display image from public folder</h1>
+            <img src="photo/img2.png" width={600} alt="sdfhjdshfjds"/>
+            <h1>display image from online link</h1>
+            <img src={myimgonline} width={600} alt="sdfsdf"/>
+            
+        </Fragment>                                                   
+
+        )
+}
+
+export default Myabout
---
+++ src/Contactus.js
@@ -0,0 +1,16 @@
+function Mycontact()
+{
+    return <h1>this is  about page</h1>
+}
+
+export default Mycontact
+
+export function Mycontact1()
+{
+    return <h2>this is my contact function 1</h2>
+}
+
+export function Mycontact2()
+{
+    return <h1>this is name export function 2</h1>
+}
